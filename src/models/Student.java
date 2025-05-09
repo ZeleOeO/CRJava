4660ca86102d4e1cab895ae9305f6f4eba3b4083
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-09 15:28:10 +0100

    fix: removed redundant id parameter

--- src/models/Student.java
+++ src/models/Student.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class Student  extends User{
     private Set<Course> courses;
 
-    public Student(int id, String name) {super(name); Admin.allStudents.add(name);}
+    public Student(String name) {super(name); Admin.allStudents.add(name);}
 
     public Map<String, String> getCoursesRegistered() {
         Map<String, String> coursesRegisteredWithTeacher = new HashMap<>();
