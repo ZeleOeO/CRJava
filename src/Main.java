413043309d1ffe912245fe9f1a22eccfc86e71dd
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-11 15:29:39 +0100

    feat: Added Teacher Menu to Application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import models.Admin;
-import models.Course;
 import models.Teacher;
 
 import java.util.Scanner;
@@ -7,11 +5,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Teacher teacher1 = new Teacher("Olufemi Peters");
-        Teacher teacher2 = new Teacher("Arijemi Paul");
-        teacher1.createCourse("CSC", 45, 300);
-        teacher2.createCourse("CSC", 50, 400);
-        System.out.println(Admin.allCourses);
+        System.out.println("Course Registration System");
+        System.out.println("Enter Your name: ");
+        String name = sc.nextLine();
+        System.out.println("Are you a teacher(t) or a student(s)");
     }
 
+    private static void teacherMenu(String name, Scanner sc, Teacher teacher) {
+        System.out.println("Create A Course (c)");
+        System.out.println("Delete A Course (d)");
+        System.out.println("View All Courses (v)");
+        System.out.println("(Press any key (but the above) to exit)");
+        String selection = sc.nextLine().toLowerCase().trim();
+        switch (selection) {
+            case "v" -> {
+                System.out.println(teacher.getAllCourseTeaching());
+                teacherMenu(name, sc, teacher);
+            }
+            case "d" -> {
+                System.out.print("Name of Course: ");
+                String deleteC = sc.nextLine().trim().toLowerCase();
+                try{
+                    teacher.deleteCourse(deleteC);
+                }
+                catch(Exception e){
+                    System.out.println(e.getMessage());
+                    System.out.println("Create a Course first");
+                    teacherMenu(name, sc, teacher);
+                }
+            }
+            case "c" -> {
+                System.out.print("Name of Course: ");
+                String cCourse = sc.nextLine().toLowerCase().toLowerCase();
+                System.out.print("Capacity of the Course: ");
+                int cCapacity = Integer.parseInt(sc.nextLine().trim().toLowerCase());
+                System.out.print("Level/Year of the Course: ");
+                int cLevel = Integer.parseInt(sc.nextLine().trim().toLowerCase());
+                teacher.createCourse(cCourse, cCapacity, cLevel);
+                System.out.println("What else do you want to do");
+                teacherMenu(name, sc, teacher);
+            }
+            default -> {}
+        }
+    }
+
+    private static void studentMenu(String name, Scanner sc) {}
 }
